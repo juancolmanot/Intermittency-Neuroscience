82552194a86b7420e798ed2c551232222a50a343
Juan <juancolmanot@gmail.com>
2023-03-15 17:06:47 -0300

    15/03/2023-17:06

--- fortrancodes/Elaskar-Del Río/Intermittency_type_III_M_function.f90
+++ fortrancodes/Elaskar-Del Río/Intermittency_type_III_M_function.f90
@@ -3,15 +3,20 @@ program Intermittency_type_III_M_function
     use mtmod
     use maps
     use matrixalgebra
+    use vector_statistics
 
     integer(sp)                 :: seed
-    integer(dp)                 :: i,j,k,n_transient,n_bins,n_reinjected
+    integer(dp)                 :: i,j,k,n_bins,n_reinjected
     real(dp)                    :: xn,xn1,c,x_start,x_end,eps(2),a(2),d(2)
-    integer(dp), allocatable    :: count(:)
-    real(dp), allocatable       :: x_bins(:),M_x(:),x_reinjected(:)
+    integer(dp), allocatable    :: counter(:)
+    real(dp), allocatable       :: x_bins(:),M_x(:),M_x_avg(:)
+    real(dp), allocatable       :: x_reinjected(:),x_reinjected_sorted(:)
+    real(dp), allocatable       :: x_avg(:)
 
     ! Datafile in which the data will be stored
-    open(12,file='Intermittency_type_III_M.dat',status='replace')
+    open(12,file='Intermittency_type_III_xreinjected.dat',status='replace')
+    open(13,file='Intermittency_type_III_M.dat',status='replace')
+    open(14,file='Intermittency_type_III_M_avg.dat',status='replace')
 
     ! Initialization of Mersenne-Twister generator
     seed = 31122_sp
@@ -21,18 +26,21 @@ program Intermittency_type_III_M_function
     c = 0.6_dp; x_start = 0._dp; x_end = c
     
     ! Number of bins in which the laminar interval will be divided
-    n_bins = 100_dp
+    n_bins = 300_dp
 
     ! Intended number of reinjected points
-    n_reinjected = 1000_dp
+    n_reinjected = 20000_dp
 
     ! Allocation and initialization of different vectors of interest
-    allocate(x_bins(n_bins),M_x(n_bins),count(n_bins),x_reinjected(n_reinjected))
-    x_bins = 0._dp; M_x = 0._dp; count = 0_dp; x_reinjected = 0._dp
+    allocate(M_x(n_reinjected))
+    allocate(x_avg(n_bins))
+    allocate(x_bins(n_bins),M_x_avg(n_bins),counter(n_bins))
+    allocate(x_reinjected(n_reinjected),x_reinjected_sorted(n_reinjected))
+    M_x = 0._dp
+    x_avg = 0._dp
+    x_bins = 0._dp; M_x_avg = 0._dp; counter = 0_dp;
+    x_reinjected = 0._dp; x_reinjected_sorted = 0._dp
     x_bins = linspace(x_start, x_end, n_bins + 1_dp)
-
-    ! Number of steps of assumed transient behavior
-    n_transient = 100_dp
 
     ! Parameters of the map
     eps = [0.01_dp, 0.0001_dp]
@@ -42,23 +50,35 @@ program Intermittency_type_III_M_function
     ! Loop through both set of parameters
     do i = 1,1
         xn = grnd()
-        write(*,*) xn
         ! Loop to obtained the desired amount of reinjection points
         k = 0_dp
         do while (k < n_reinjected)
             xn1 = map_III_a(xn,eps(i),a(i),d(i))
-            if (j > n_transient) then
-                write(*,*) "Got passed transient"
-                if (xn1 > 0._dp .and. xn1 <= c .and. abs(xn) > abs(c)) then
-                    write(*,*) "Got a reinjected point",k
-                    k = k + 1_dp
-                    x_reinjected(k) = xn1
-                endif
+            if (xn1 > 0._dp .and. xn1 <= c .and. abs(xn) > abs(c)) then
+                k = k + 1_dp
+                x_reinjected(k) = xn1
+                write(12,"(I8,E14.4)") k,xn1
             endif
             xn = xn1
         enddo
+        ! Sort vector from min to max value
+        x_reinjected_sorted = sort_minor(x_reinjected)
+
+        ! Compute the M function with all points
         do j = 1,n_reinjected
-            write(12,"(I8,E16.6)") j,x_reinjected(j)
+            do k = 1,j
+                M_x(j) = M_x(j) + x_reinjected_sorted(k)
+            enddo
+            M_x(j) = M_x(j)/real(j,dp)
+            write(13,"(2(E16.6))") x_reinjected_sorted(j),M_x(j)
+        enddo
+        x_avg = x_n_avg(x_reinjected_sorted,n_bins)
+        do j = 1,n_bins
+            do k = 1,j
+                M_x_avg(j) = M_x_avg(j) + x_avg(k)
+            enddo
+            M_x_avg(j) = M_x_avg(j)/real(j,dp)
+            write(14,"(I8,2(E16.6))") j,x_avg(j),M_x_avg(j)
         enddo
     enddo
 
