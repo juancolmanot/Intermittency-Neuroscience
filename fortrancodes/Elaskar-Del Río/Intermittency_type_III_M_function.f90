5c1b34a40bd044f12c803d7216ba08fcad62cb8a
Juan <juancolmanot@gmail.com>
2023-03-17 17:37:27 -0300

    17/03/2023-17:37

--- fortrancodes/Elaskar-Del Río/Intermittency_type_III_M_function.f90
+++ fortrancodes/Elaskar-Del Río/Intermittency_type_III_M_function.f90
@@ -14,22 +14,25 @@ program Intermittency_type_III_M_function
     real(dp), allocatable       :: x_avg(:)
 
     ! Datafile in which the data will be stored
-    open(12,file='Intermittency_type_III_xreinjected.dat',status='replace')
-    open(13,file='Intermittency_type_III_M.dat',status='replace')
-    open(14,file='Intermittency_type_III_M_avg.dat',status='replace')
+    open(12,file='Intermittency_type_III_xreinjected_case1.dat',status='replace')
+    open(13,file='Intermittency_type_III_M_case1.dat',status='replace')
+    open(14,file='Intermittency_type_III_M_avg_case1.dat',status='replace')
+    open(15,file='Intermittency_type_III_xreinjected_case2.dat',status='replace')
+    open(16,file='Intermittency_type_III_M_case2.dat',status='replace')
+    open(17,file='Intermittency_type_III_M_avg_case2.dat',status='replace')
 
     ! Initialization of Mersenne-Twister generator
     seed = 31122_sp
     call sgrnd(seed)
 
     ! Limits of the laminar interval
-    c = 0.6_dp; x_start = 0._dp; x_end = c
+    c = 0.6_dp;x_start = 0._dp; x_end = c
     
     ! Number of bins in which the laminar interval will be divided
-    n_bins = 300_dp
+    n_bins = 100_dp
 
     ! Intended number of reinjected points
-    n_reinjected = 20000_dp
+    n_reinjected = 10000_dp
 
     ! Allocation and initialization of different vectors of interest
     allocate(M_x(n_reinjected))
@@ -40,7 +43,7 @@ program Intermittency_type_III_M_function
     x_avg = 0._dp
     x_bins = 0._dp; M_x_avg = 0._dp; counter = 0_dp;
     x_reinjected = 0._dp; x_reinjected_sorted = 0._dp
-    x_bins = linspace(x_start, x_end, n_bins + 1_dp)
+    !x_bins = linspace(x_start, x_end, n_bins)
 
     ! Parameters of the map
     eps = [0.01_dp, 0.0001_dp]
@@ -49,6 +52,7 @@ program Intermittency_type_III_M_function
 
     ! Loop through both set of parameters
     do i = 1,1
+        !write(*,*) i
         xn = grnd()
         ! Loop to obtained the desired amount of reinjection points
         k = 0_dp
@@ -57,30 +61,48 @@ program Intermittency_type_III_M_function
             if (xn1 > 0._dp .and. xn1 <= c .and. abs(xn) > abs(c)) then
                 k = k + 1_dp
                 x_reinjected(k) = xn1
-                write(12,"(I8,E14.4)") k,xn1
+                if (i == 1) then
+                    write(12,"(I8,E14.4)") k,xn1
+                else if (i == 2) then
+                    write(15,"(I8,E14.4)") k,xn1
+                endif
             endif
             xn = xn1
         enddo
         ! Sort vector from min to max value
         x_reinjected_sorted = sort_minor(x_reinjected)
 
-        ! Compute the M function with all points
+        ! Compute the M function
         do j = 1,n_reinjected
             do k = 1,j
                 M_x(j) = M_x(j) + x_reinjected_sorted(k)
             enddo
-            M_x(j) = M_x(j)/real(j,dp)
-            write(13,"(2(E16.6))") x_reinjected_sorted(j),M_x(j)
+            if (i == 1_dp) then
+                write(13,"(2(E16.6))") x_reinjected_sorted(j),M_x(j)/real(j,dp)
+            else if (i == 2_dp) then
+                write(16,"(2(E16.6))") x_reinjected_sorted(j),M_x(j)/real(j,dp)
+            endif
         enddo
+
+        ! 
         x_avg = x_n_avg(x_reinjected_sorted,n_bins)
+
+        ! Compute M function with averages
         do j = 1,n_bins
             do k = 1,j
-                M_x_avg(j) = M_x_avg(j) + x_avg(k)
+                M_x_avg(j) = M_x_avg(j) + x_avg(j)
             enddo
-            M_x_avg(j) = M_x_avg(j)/real(j,dp)
-            write(14,"(I8,2(E16.6))") j,x_avg(j),M_x_avg(j)
+            if (i == 1_dp) then
+                write(14,"(2(E16.6))") x_avg(j),M_x_avg(j)/real(j,dp)
+            else if (i == 2_dp) then
+                write(17,"(2(E16.6))") x_avg(j),M_x_avg(j)/real(j,dp)
+            endif
         enddo
+
+        M_x = 0._dp; counter = 0_dp; M_x_avg = 0._dp; x_reinjected = 0._dp; x_reinjected_sorted = 0._dp
     enddo
+
+    
 
 
 end program Intermittency_type_III_M_function
