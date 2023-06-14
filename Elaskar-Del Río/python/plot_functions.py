19fce7305e3541e4b7313887dfb269a0534841af
Juan <juancolmanot@gmail.com>
2023-06-14 10:40:27 -0300

    14/06/2023 - 10:40

--- Elaskar-Del Río/python/plot_functions.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-def animate(x, y, ax):
-    
-    ax.clear()
-    ax.plot(x, y)
--- Elaskar-Del Río/python/stats.py
+++
@@ -1,3 +0,0 @@
-def rel_err(a, b):
-
-    return abs((b - a) / a)
