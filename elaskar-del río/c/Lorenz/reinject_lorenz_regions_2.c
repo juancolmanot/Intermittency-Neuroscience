36ba471e3b24d8a0708ab5ed305ffe3430d334cc
root <juancolmanot@gmail.com>
2023-10-25 17:08:32 -0300

    25/10/2023 - 17:08

--- elaskar-del río/c/Lorenz/reinject_lorenz_regions_2.c
+++ elaskar-del río/c/Lorenz/reinject_lorenz_regions_2.c
@@ -50,7 +50,12 @@ int main(void) {
     /* Passes counter */
     unsigned int counter = 0;
     unsigned int pass_target = 800000;
-    unsigned int reinject_target = 100000;
+    unsigned int reinject_target = 200000;
+
+    double *yreinj, *yreinj_prev;
+
+    yreinj = calloc(reinject_target, sizeof(double));
+    yreinj_prev = calloc(reinject_target, sizeof(double));
 
     /* Arrays for x, y, z */
     double xi[3], yi[3], zi[3];
@@ -60,11 +65,6 @@ int main(void) {
     double xfit[3], yfit[3], zfit[3];
     double *coefficients = calloc(3, sizeof(double));
     double a_c, b_c, c_c;
-    // double *yreg, *zreg;
-    // yreg = calloc(pass_target, sizeof(double));
-    // zreg = calloc(pass_target, sizeof(double));
-    // gsl_matrix *ty = gsl_matrix_alloc(2, pass_target);
-    // gsl_matrix *tz = gsl_matrix_alloc(2, pass_target);
 
     /* Laminar region */
     FILE *region_1, *region_2, *region_3, *region_4, *region_5;
@@ -77,13 +77,13 @@ int main(void) {
     double clam = 1.85;
     unsigned int count_reinject = 0;
     double yr0, yr1, yr2, yr3, yr4, yr5, yr6;
-    yr0 = 5;
+    yr0 = 0;
     yr1 = 18;
     yr2 = 25;
     yr3 = 30;
     yr4 = yf - clam;
     yr5 = yf + clam;
-    yr6 = 70;
+    yr6 = 100;
 
     unsigned int counter1, counter2, counter3, counter4, counter5;
 
@@ -92,6 +92,30 @@ int main(void) {
     unsigned int counter_tot;
 
     counter_tot = counter1 + counter2 + counter3 + counter4 + counter5;
+
+    FILE *weights = fopen("../datafiles/rpd_weights.dat", "w");
+
+    fprintf(weights, "%d\n", counter1);
+    fprintf(weights, "%d\n", counter2);
+    fprintf(weights, "%d\n", counter3);
+    fprintf(weights, "%d\n", counter4);
+    fprintf(weights, "%d\n", counter5);
+
+    FILE *RPD1, *RPD2, *RPD3, *RPD4, *RPD5;
+    RPD1 = fopen("../datafiles/rpd_lorenz_1.dat", "w");
+    RPD2 = fopen("../datafiles/rpd_lorenz_2.dat", "w");
+    RPD3 = fopen("../datafiles/rpd_lorenz_3.dat", "w");
+    RPD4 = fopen("../datafiles/rpd_lorenz_4.dat", "w");
+    RPD5 = fopen("../datafiles/rpd_lorenz_5.dat", "w");
+    unsigned int n_bins = 200;
+    double ymin, ymax, yj, dy;
+    ymin = yf - clam;
+    ymax = yf + clam;
+    dy = (ymax - ymin) / (double)(n_bins - 1);
+    yj = ymin;
+    double *yreinjected1, *yreinjected2, *yreinjected3, *yreinjected4, *yreinjected5;
+    
+    double knorm = 0;
 
     /* Poincaré section */
     double xp = 0;
@@ -119,7 +143,7 @@ int main(void) {
         xi[1] = xi[2];
         xi[2] = x[0];
         
-        yi_prev = yi[1];
+        yi[0] = yi[1];
         yi[1] = yi[2];
         yi[2] = x[1];
 
@@ -151,6 +175,8 @@ int main(void) {
                     // c_c = coefficients[2];
                     if (yi[1] >= yf - clam && yi[1] <= yf + clam) {
                         if (yi_prev > yf + clam || yi_prev < yf - clam) {
+                            yreinj[count_reinject] = yi[1];
+                            yreinj_prev[count_reinject] = yi_prev;
                             count_reinject++;
                             fprintf(reinjected, "%5.15f\n", yi[1]);
                             if (yi_prev > yr0 && yi_prev < yr1) {
@@ -188,6 +214,7 @@ int main(void) {
                         printf("counter 3: %d\n", counter3);
                         printf("counter 4: %d\n", counter4);
                         printf("counter 5: %d\n", counter5);
+                        printf("counter tot: %d\n", counter_tot);
                     }
                     yi_prev = yi[1];
                 }
@@ -195,71 +222,46 @@ int main(void) {
         }
     }
 
-    // FILE *weights = fopen("../datafiles/rpd_weights.dat", "w");
+    yreinjected1 = calloc(n_bins, sizeof(double));
+    yreinjected2 = calloc(n_bins, sizeof(double));
+    yreinjected3 = calloc(n_bins, sizeof(double));
+    yreinjected4 = calloc(n_bins, sizeof(double));
+    yreinjected5 = calloc(n_bins, sizeof(double));
 
-    // fprintf(weights, "%d\n", counter1);
-    // fprintf(weights, "%d\n", counter2);
-    // fprintf(weights, "%d\n", counter3);
-    // fprintf(weights, "%d\n", counter4);
-    // fprintf(weights, "%d\n", counter5);
+    /* Increment y from min to max to generate bins */
+    for (unsigned int i = 0; i < n_bins; i++) {
 
-    // FILE *RPD1, *RPD2, *RPD3, *RPD4, *RPD5;
-    // RPD1 = fopen("../datafiles/rpd_lorenz_1.dat", "w");
-    // RPD2 = fopen("../datafiles/rpd_lorenz_2.dat", "w");
-    // RPD3 = fopen("../datafiles/rpd_lorenz_3.dat", "w");
-    // RPD4 = fopen("../datafiles/rpd_lorenz_4.dat", "w");
-    // RPD5 = fopen("../datafiles/rpd_lorenz_5.dat", "w");
-    // unsigned int n_bins = 200;
-    // double ymin, ymax, yj, dy;
-    // ymin = yf - clam;
-    // ymax = yf + clam;
-    // dy = (ymax - ymin) / (double)(n_bins - 1);
-    // yj = ymin;
-    // double *yreinjected1, *yreinjected2, *yreinjected3, *yreinjected4, *yreinjected5;
-    // yreinjected1 = calloc(n_bins, sizeof(double));
-    // yreinjected2 = calloc(n_bins, sizeof(double));
-    // yreinjected3 = calloc(n_bins, sizeof(double));
-    // yreinjected4 = calloc(n_bins, sizeof(double));
-    // yreinjected5 = calloc(n_bins, sizeof(double));
-    // double knorm = 0;
-    
-    // /* Increment y from min to max to generate bins */
-    // for (unsigned int i = 0; i < n_bins; i++) {
-
-    //     /* Loop through whole array of y fitted */
-    //     for (unsigned int j = 1; j < pass_target; j++) {
+        /* Loop through whole array of y fitted */
+        for (unsigned int j = 1; j < reinject_target - 1; j++) {
             
-    //         /* Ask for belonging to bin and reinjection */
-    //         if (yreg[j] > yj && yreg[j] <= yj + dy) {
-    //             if (yreg[j] >= yf - clam && yreg[j] <= yf + clam) {
-    //                 if (yreg[j - 1] < yf - clam || yreg[j - 1] > yf + clam) {
-    //                     if (yreg[j - 1] > yr0 && yreg[j - 1] < yr1) {
-    //                         yreinjected1[i]++;
-    //                     }
-    //                     if (yreg[j - 1] > yr1 && yreg[j - 1] < yr2) {
-    //                         yreinjected2[i]++;
-    //                     }
-    //                     if (yreg[j - 1] > yr2 && yreg[j - 1] < yr3) {
-    //                         yreinjected3[i]++;
-    //                     }
-    //                     if (yreg[j - 1] > yr3 && yreg[j - 1] < yr4) {
-    //                         yreinjected4[i]++;
-    //                     }
-    //                     if (yreg[j - 1] > yr5 && yreg[j - 1] < yr6) {
-    //                         yreinjected5[i]++;
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
+            /* Ask for belonging to bin */
+            if (yreinj[j] > yj && yreinj[j] <= yj + dy) {
+                
+                if (yreinj_prev[j] > yr0 && yreinj_prev[j] < yr1) {
+                    yreinjected1[i]++;
+                }
+                if (yreinj_prev[j] > yr1 && yreinj_prev[j] < yr2) {
+                    yreinjected2[i]++;
+                }
+                if (yreinj_prev[j] > yr2 && yreinj_prev[j] < yr3) {
+                    yreinjected3[i]++;
+                }
+                if (yreinj_prev[j] > yr3 && yreinj_prev[j] < yr4) {
+                    yreinjected4[i]++;
+                }
+                if (yreinj_prev[j] > yr5 && yreinj_prev[j] < yr6) {
+                    yreinjected5[i]++;
+                }
+            }
+        }
         
-    //     fprintf(RPD1, "%5.15f  %5.15f\n", yj, yreinjected1[i]);
-    //     fprintf(RPD2, "%5.15f  %5.15f\n", yj, yreinjected2[i]);
-    //     fprintf(RPD3, "%5.15f  %5.15f\n", yj, yreinjected3[i]);
-    //     fprintf(RPD4, "%5.15f  %5.15f\n", yj, yreinjected4[i]);
-    //     fprintf(RPD5, "%5.15f  %5.15f\n", yj, yreinjected5[i]);
-    //     yj += dy;
-    // }
+        fprintf(RPD1, "%5.15f  %5.15f\n", yj, yreinjected1[i]);
+        fprintf(RPD2, "%5.15f  %5.15f\n", yj, yreinjected2[i]);
+        fprintf(RPD3, "%5.15f  %5.15f\n", yj, yreinjected3[i]);
+        fprintf(RPD4, "%5.15f  %5.15f\n", yj, yreinjected4[i]);
+        fprintf(RPD5, "%5.15f  %5.15f\n", yj, yreinjected5[i]);
+        yj += dy;
+    }
 
     gsl_odeiv2_evolve_free(e);
     gsl_odeiv2_control_free(c);
@@ -270,16 +272,16 @@ int main(void) {
     fclose(region_2);
     fclose(region_3);
     fclose(region_4);
-    // fclose(RPD1);
-    // fclose(RPD2);
-    // fclose(RPD3);
-    // fclose(RPD4);
+    fclose(RPD1);
+    fclose(RPD2);
+    fclose(RPD3);
+    fclose(RPD4);
     fclose(reinjected);
     fclose(reinjected1);
     fclose(reinjected2);
     fclose(reinjected3);
     fclose(reinjected4);
     fclose(reinjected5);
-    // fclose(weights);
+    fclose(weights);
     return 0;
 }
